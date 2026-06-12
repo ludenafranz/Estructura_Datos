6c3c8989b3670bacf91514b78b1ddff157b1a7fe
ludenafranz <franz.ludena@unl.edu.ec>
2026-06-12 13:07:58 -0500

    feat: Learning Javascrip for noobscirs

---
+++ Unidad2/00javascript/Tipos-dartos.js
@@ -0,0 +1,32 @@
+
+let string = "Hola Mundo";
+
+let number = 42;
+
+//booleano
+let boolean = true;
+
+//valores nulos 
+let nohaydatos = null;
+
+//indefinido 
+let indefinido ;
+
+//simbolo valor unico
+let simbolo = Sumbol('unico');
+
+//datos bigint
+let numerogrande = 2n;
+
+//objetos
+let carro = {
+    marca: "Toyota",
+    modelo: "Corolla",
+    año: 2020
+};
+
+let array = [1,2,3,4,5,6];
+
+function saludar(){
+    console.log("hola");
+}
---
+++ Unidad2/00javascript/string.js
@@ -0,0 +1,13 @@
+let string1 = 'Hola'
+let string = `${string1} feliz :)`
+
+console.log(string)
+
+
+let frase = 'Javascrip es genial'
+let bajo = frase.toLowerCase
+console.log(bajo);
+console.log(frase.length);
+console.log(frase.toLocaleLowerCase())
+console.log(frase.toUpperCase())
+console.log(frase.substring(0,10))
